d8680d6b111579490042a677f099bec990a9b7c8
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-07-02 17:36:49 +0530

    initial commit

---
+++ weather app/WeatherButton.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherButton.swift
+//  weather app
+//
+//  Created by  GPCODERS on 02/07/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
